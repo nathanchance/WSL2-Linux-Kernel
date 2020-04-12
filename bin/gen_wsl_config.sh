5560381ad015b2cf31277a37127d95b4f813c844
Nathan Chancellor <natechancellor@gmail.com>
2020-04-11 21:07:23 -0700

    x86/configs: Add slightly tuned wsl2_defconfig
    Generated by:
    
    $ ./bin/gen_wsl_config.sh
    
    Signed-off-by: Nathan Chancellor <natechancellor@gmail.com>

---
+++ bin/gen_wsl_config.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+BASE=$(cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"/.. && pwd)
+cd "${BASE}" || exit ${?}
+
+curl -LSso arch/x86/configs/wsl2_defconfig https://github.com/microsoft/WSL2-Linux-Kernel/raw/linux-msft-wsl-4.19.y/Microsoft/config-wsl
+
+./scripts/config --file arch/x86/configs/wsl2_defconfig \
+                 --disable FTRACE \
+                 --disable MODULES \
+                 --disable STACK_VALIDATION \
+                 --disable UNWINDER_ORC \
+                 --enable LTO_CLANG \
+                 --enable UNWINDER_FRAME_POINTER \
+                 --set-val LOCALVERSION \"-microsoft-cbl\" \
+                 --undefine FRAME_WARN
+
+./bin/build.sh -u
